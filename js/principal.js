18135f52986105634ed9c0da49c081ef721f3ad5
nicolas borba <borbanicolasmk@gmail.com>
2022-01-05 22:12:51 -0300

    aula2

--- js/principal.js
+++ js/principal.js
@@ -1,3 +1,15 @@
 var titulo = document.querySelector('.titulo')
 titulo.textContent = 'aparecida nutricionista'
 
+var paciente = document.querySelector("#primeiroPaciente")
+var tdPeso = document.querySelector(".info-peso")
+var tdAltura = document.querySelector(".info-altura")
+
+var peso = tdPeso.textContent
+var altura = tdAltura.textContent
+
+var imc = peso / (altura * altura)
+
+var tdImc = document.querySelector(".info-imc")
+
+tdImc.textContent = imc
