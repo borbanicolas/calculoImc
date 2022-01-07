50a46e98bb725c33b1a105d7a286dfaca84d5319
nicolas borba <borbanicolasmk@gmail.com>
2022-01-06 21:23:09 -0300

    replicando validacao eo calculo

--- js/principal.js
+++ js/principal.js
@@ -1,15 +1,32 @@
 var titulo = document.querySelector('.titulo')
 titulo.textContent = 'aparecida nutricionista'
 
-var paciente = document.querySelector("#primeiroPaciente")
-var tdPeso = document.querySelector(".info-peso")
-var tdAltura = document.querySelector(".info-altura")
+var pacientes = document.querySelectorAll(".paciente")
 
-var peso = tdPeso.textContent
-var altura = tdAltura.textContent
+for (var i = 0 ; i < pacientes.length; i++){
+    var paciente = pacientes[i]
 
-var imc = peso / (altura * altura)
-
-var tdImc = document.querySelector(".info-imc")
-
-tdImc.textContent = imc
+    var tdPeso = paciente.querySelector(".info-peso")
+    var tdAltura = paciente.querySelector(".info-altura")
+    var tdImc = paciente.querySelector(".info-imc")    
+    var peso = tdPeso.textContent
+    var altura = tdAltura.textContent
+    
+    var alturaEhValida = true
+    var pesoEhValido = true
+    if(peso <= 0 || peso > 1000){
+        console.log('peso invalido')
+        tdImc.textContent = 'peso invalido'
+        pesoEhValido = false
+    }
+    if(altura <= 0 || altura >= 3){
+        console.log('altura e invalida')
+        tdImc.textContent = ' altura invalida'
+        alturaEhValida = false
+    }
+    if(alturaEhValida && pesoEhValido){
+    var imc = peso / (altura * altura)
+    tdImc.textContent = imc.toFixed(2)
+    }
+    
+}
